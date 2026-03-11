e8e042950a953ffd30db6dbd6709175da1966e7a
recepyalcinkaya <137687914+recepyalcinkaya@users.noreply.github.com>
2026-03-11 11:02:08 +0000

    Deploying to gh-pages from @ recepyalcinkaya/recepyalcinkaya.github.io@12a7ddf1861b29d705980ad4cda1848314d51c04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of my automotive software, ADAS, and edge AI projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
