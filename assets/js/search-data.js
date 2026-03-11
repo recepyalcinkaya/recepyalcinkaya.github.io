67fd3928c589b8f8519ba1f8eba6c22fdc405e5a
recepyalcinkaya <137687914+recepyalcinkaya@users.noreply.github.com>
2026-03-11 11:08:32 +0000

    Deploying to gh-pages from @ recepyalcinkaya/recepyalcinkaya.github.io@7ce32760712cf6cef17f39b370e85fd606c08fe4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-traffic-sign-amp-light-recognition",
-          title: 'Traffic Sign &amp;amp; Light Recognition',
-          description: "Real-time detection optimized for edge hardware (Raspberry Pi 5).",
+          section: "News",},{id: "projects-traffic-sign-recognition",
+          title: 'Traffic Sign Recognition',
+          description: "Real-time traffic sign detection optimized for edge hardware (Raspberry Pi 5).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_traffic_sign/";
             },},{id: "projects-lane-tracking-system-lts",
