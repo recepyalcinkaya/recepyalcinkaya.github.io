b00a168aa732b936f1b97843a625923e8a46114e
recepyalcinkaya <137687914+recepyalcinkaya@users.noreply.github.com>
2026-03-11 10:02:48 +0000

    Deploying to gh-pages from @ recepyalcinkaya/recepyalcinkaya.github.io@17a65cadcf74d09c09bb6deb48c79cd8c2b47a8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,6 +456,11 @@ ninja.data = [{
           description: "Real-time detection optimized for edge hardware (Raspberry Pi 5).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_traffic_sign/";
+            },},{id: "projects-lane-tracking-system-lts",
+          title: 'Lane Tracking System (LTS)',
+          description: "Real-time lane detection and course correction using OpenCV.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_lane_tracking/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
